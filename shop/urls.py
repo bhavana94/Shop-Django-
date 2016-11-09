0e28f74964dd43e59e69dc1d02d0f66a67a0cf9a
bhavana94 <bhavanasingh49@gmail.com>
2016-11-10 01:23:36 +0530

    quick fix

--- shop/urls.py
+++ shop/urls.py
@@ -11,6 +11,6 @@ urlpatterns = [
     url(r'^logout/$', views.signout, name="logout"),
     url(r'^seller/$', views.seller_view_item, name='seller-view-item'),
     url(r'^seller/add-item/$', views.seller_add_item, name='seller-add-item'),
-    url(r'^customer/(?P<slug>.*)/$', views.order_details, name='order_details'),
-    url(r'^customer/$', views.customer_order, name='customer_order'),
+    url(r'^customer/(?P<slug>.*)/$', views.order_details, name='order-details'),
+    url(r'^customer/$', views.customer_order, name='customer-order'),
 ]
